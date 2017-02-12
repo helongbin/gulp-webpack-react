d5b24f1cc65cda2835b091d9e5df43b1fd14bb14
Robin He <csuhlb@gmail.com>
2017-02-12 20:00:49 +0800

    add postcss and autoprefixer

--- src/js/about/app.js
+++ src/js/about/app.js
@@ -4,4 +4,13 @@
  */
 import "../../../src/less/about.less";
 
-console.log(123789456);
+
+var a = [];
+for (let i = 0; i < 10; i++) {
+    a[i] = function () {
+        console.log(i);
+    };
+}
+a[5](); // 6
+
+var aaa = 9999;
